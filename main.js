5e7a848e61a928b834dfdf572221b8d8ff801c48
Fredrik Lautrup <fredrik@lautrup.se>
2016-12-18 18:51:45 +0100

    Working Wifi

--- main.js
+++ main.js
@@ -10,47 +10,50 @@ const { name: WIFI_NAME, password: WIFI_PASSWORD } = CONFIG
 
 // Wrap the call to wifi.connect in a native Promise so it's a bit easier to deal with later
 const connect = (networkName, options) => {
-  console.log(`attempting to connect to network named: ${ networkName }`)
+  console.log(`attempting to connect to network named: ${networkName}`)
   return new Promise((resolve, reject) => wifi.connect(networkName, options, error => {
-      const ipAddress = wifi.getIP().ip
-      if (error) reject(error)
-      else resolve(ipAddress)
-    })
+    const ipAddress = wifi.getIP().ip
+    if (error) reject(error)
+    else resolve(ipAddress)
+  })
   )
 }
 
-const lightOn = () =>{
-digitalWrite(D2, isOn = false)
-console.log('A blue LED should be on...')
+const lightOn = () => {
+  digitalWrite(D2, isOn = false)
+  console.log('A blue LED should be on...')
 }
 
 const lightOff = () => {
-digitalWrite(D2, isOn = true)
-console.log(`The blue light should be off...`)
+  digitalWrite(D2, isOn = true)
+  console.log(`The blue light should be off...`)
 }
 
-// Save a reference to the attempt that we can pass around.
-const connectionAttempt = connect(WIFI_NAME, { password: WIFI_PASSWORD })
-.then(ip => {
-  console.log(`successfully connected to ${ ip }`)
-  lightOn() // Again, this is just a reminder that we are wifi connected now
-})
-.then(() => {
-  // From here on you have wifi access.
-  // You can do all your app stuff in this or subsequent .then() blocks.
-  // One good idea is to set up a "disconnected" event listener that allows you to handle
-  // lost wifi
-  wifi.on('disconnected', () => {
-    lightOff()
-    console.log(`successfully disconnected...`)
-  })
-})
-// Here's an example of some app code that comes after we've set up all our wifi stuff.
-// In this case I just trigger a disconnect for demonstration purposes.
-// But you could have all your app's business logic playout here.
-.then(() => setTimeout(wifi.disconnect, 3000) )
-.catch(error => {
-  console.error(error)
-})
+function main() {
+  wifi.stopAP() // Don't act as a Wifi access point for other devices
+  // Save a reference to the attempt that we can pass around.
+  const connectionAttempt = connect(WIFI_NAME, { password: WIFI_PASSWORD })
+    .then(ip => {
+      console.log(`successfully connected to ${ip}`)
+      lightOn() // Again, this is just a reminder that we are wifi connected now
+    })
+    .then(() => {
+      // From here on you have wifi access.
+      // You can do all your app stuff in this or subsequent .then() blocks.
+      // One good idea is to set up a "disconnected" event listener that allows you to handle
+      // lost wifi
+      wifi.on('disconnected', () => {
+        lightOff()
+        console.log(`successfully disconnected...`)
+      })
+    })
+    // Here's an example of some app code that comes after we've set up all our wifi stuff.
+    // In this case I just trigger a disconnect for demonstration purposes.
+    // But you could have all your app's business logic playout here.
+    //.then(() => setTimeout(wifi.disconnect, 3000))
+    //.catch(error => {
+    //  console.error(error)
+    //})
 
-wifi.stopAP() // Don't act as a Wifi access point for other devices
+  
+}
