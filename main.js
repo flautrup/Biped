2e66d79f29f4bb7261f8df1bb21f520249d86abf
Fredrik Lautrup <fredrik@lautrup.se>
2017-01-06 17:10:35 +0100

    Working REST API for servo and output.

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 // main.js
 const wifi = require('Wifi')  // This is one of our magic, native Espruino friends.
+//const servo = require('servo') // Load native servo module
 
 // If you plan to publish your code,
 // it's a good idea to keep your wifi name and password in a secure file that you do not version control.
@@ -29,27 +30,101 @@ const lightOff = () => {
   console.log(`The blue light should be off...`)
 }
 
+const testServo = (port) => {
+  move(port,0,function () {
+    move(port,1, function () {
+      move(port,.5, function () {
+        move(port,0)
+      })
+    })
+  })
+}
+
+const move = (port, value, callback) => {
+  var steps = 0;
+
+  console.log(value);
+
+  interval = setInterval(function () {
+    if (steps > 20) {
+      clearInterval(interval);
+      interval = undefined;
+      steps = 0;
+      if (callback) callback();
+    }
+    digitalPulse(port, 1, 1 + E.clip(value, 0, 1));
+    //digitalPulse(port, 1, 1.5);
+    steps++;
+  }, 20);
+}
+
+const httpResp = (res, message) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end(message);
+  console.log(message);
+}
+
+var parseREST = (req) => {
+  var RESTReq = {};
+  var a = url.parse(req.url, true);
+
+  var pathParts = a.pathname.split("/");
+  //console.log(pathParts);
+
+  if (pathParts.length == 2 && pathParts[1] == "") {
+    RESTReq.action = "default";
+  }
+
+  if (pathParts.length > 1) {
+    RESTReq.action = pathParts[1];
+  }
+
+  if (pathParts.length > 2) {
+    RESTReq.port = pathParts[2];
+  }
+
+  if (pathParts.length > 3) {
+    RESTReq.value = pathParts[3];
+  }
+
+  console.log(JSON.stringify(RESTReq));
+  return RESTReq;
+
+}
+
 
 
 function onPageRequest(req, res) {
-  var a = url.parse(req.url, true);
-  if (a.pathname == "/") {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end("BIPed REST API 0.1");
-  } else if (a.pathname == "/hello") {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end("Hello World");
-  } else if (a.pathname == "/ledOn") {
+  //Path mapping to function for REST API
+  var RESTReq = parseREST(req);
+
+  if (RESTReq.action == "default") {
+    httpResp(res, "BIPed REST API 0.1");
+  } else if (RESTReq.action == "hello") {
+    httpResp(res, "Hello World")
+  } else if (RESTReq.action == "ledOn") {
     digitalWrite(D0, false);
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end("Red Led On");
-  } else if (a.pathname == "/ledOff") {
+    httpResp(res, "Red Led On");
+  } else if (RESTReq.action == "ledOff") {
     digitalWrite(D0, true);
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end("Red Led Off");
+    httpResp(res, "Red Led Off");
+  } else if (RESTReq.action == "wifi") {
+    httpResp(res, JSON.stringify(wifi.getStatus()) + JSON.stringify(wifi.getIP()));
+  } else if (RESTReq.action == "write") {
+    digitalWrite(RESTReq.port, RESTReq.value);
+    httpResp(res, JSON.stringify(RESTReq));
+  } else if (RESTReq.action == "servo") {
+    if (RESTReq.value == "test") {
+      testServo(RESTReq.port);
+    } else if (RESTReq.value > 0) {
+      move(RESTReq.port, RESTReq.value / 10);
+    } else {
+      move(RESTReq.port, 0);
+    }
+    httpResp(res, JSON.stringify(RESTReq));
   } else {
     res.writeHead(404, { 'Content-Type': 'text/plain' });
-    res.end(JSON.stringify(a));
+    res.end(JSON.stringify(RESTReq));
   }
 }
 
@@ -73,6 +148,7 @@ function main() {
 
       var http = require("http");
       http.createServer(onPageRequest).listen(8080);
+
     })
   // Here's an example of some app code that comes after we've set up all our wifi stuff.
   // In this case I just trigger a disconnect for demonstration purposes.
