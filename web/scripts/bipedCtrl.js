1c40001e03d88d3d49e699cb6440bccc09cbf157
Fredrik Lautrup <fredrik@lautrup.se>
2017-01-14 23:02:31 +0100

    Added record and play functionality to web client

--- web/scripts/bipedCtrl.js
+++ web/scripts/bipedCtrl.js
@@ -7,7 +7,23 @@ angular.module('BiPed', ['ngMaterial'])
     D14: 50
   };
 
+  $scope.flow=[];
+
   $scope.server="192.168.1.115:8080"
+
+  $scope.store = (state) => {
+    var cloneOfState = JSON.parse(JSON.stringify(state));
+    $scope.flow.push(cloneOfState);
+  }
+
+  $scope.play = (flow) => {
+    for(var count=0 ; count < flow.length; count++) {
+      $scope.updateServo("D4",flow[count].D4);
+      $scope.updateServo("D5",flow[count].D5);
+      $scope.updateServo("D12",flow[count].D12);
+      $scope.updateServo("D14",flow[count].D14);
+    }
+  }
 
   $scope.updateServo = (port, value) => {
       url="http://"+$scope.server+"/servo/"+port+"/"+value;
